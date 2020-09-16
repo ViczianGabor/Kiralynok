62c32b81ceab02702373066a47c49969415957ab
ViczianGabor <viczian.gabor13@gmail.com>
2020-09-16 08:28:52 +0200

    Add project files.

---
+++ Kiralynok/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiralynok
+{
+    class tabla
+    {
+        private char[,] t = new char[8,8];
+        private char Urescella;
+        private int uresOszlopSzam;
+        private int UresSorokszama;
+
+        public void elhelyez()
+        {
+
+        }
+
+        public void Megjelenit()
+        {
+
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            tabla k = new tabla();
+            
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
