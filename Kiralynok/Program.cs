c5264e8c5922701294e373fb46d7bc7299a50620
ViczianGabor <viczian.gabor13@gmail.com>
2020-09-16 09:01:48 +0200

    return 0

--- Kiralynok/Program.cs
+++ Kiralynok/Program.cs
@@ -8,28 +8,71 @@ namespace Kiralynok
 {
     class tabla
     {
-        private char[,] t = new char[8,8];
+        private char[,] t;
         private char Urescella;
         private int uresOszlopSzam;
         private int UresSorokszama;
+
+
+
+
+
+
+        public tabla(char ch)
+        {
+            t = new char[8, 8];
+            Urescella = ch;
+            for (int i = 0; i < 8; i++)
+            {
+                
+                for (int j = 0; j < 8; j++)
+                {
+                    t[i, j] = Urescella;
+                }
+            }
+        }
+
 
         public void elhelyez()
         {
 
         }
 
-        public void Megjelenit()
+
+        public void fajlbair()
+        {
+
+
+
+        }
+        public void megjelenit()
         {
 
 
         }
+
+
+
+        public int uresoszlop()
+        {
+            return 0;
+        }
+
+        public int uresSor()
+        {
+            return 0;
+
+
+        }
+
+
     }
     class Program
     {
         static void Main(string[] args)
         {
             
-            tabla k = new tabla();
+            //tabla k = new tabla();
             
 
 
